1d2884d7ea3133e7ae8d676aaf0e0b08124d1b53
Spyros Trimis <spyrostrimis@gmail.com>
2023-04-14 13:18:57 +0300

    signup.js component and its css

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 // import './App.css';
+import Signup from "./Components/Signup";
 
 import MoviesForm from "./Components/MoviesForm";
 
 function App() {
   return (
     <div className="App">
+      {/* <Signup /> */}
       <MoviesForm/>
     </div>
   );
