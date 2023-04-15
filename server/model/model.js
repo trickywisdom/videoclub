6ddda182ddb342b4a410dff1d020ead4f723ef20
Ioannis Skardasis <iskardasis@hotmail.com>
2023-04-15 14:33:18 +0300

    changes to schema and routers

--- server/model/model.js
+++ server/model/model.js
@@ -1,25 +1,25 @@
 const mongoose = require("mongoose");
 
 // Define article model schema
-const movieSchema = new mongoose.Schema(
-  {
-    id: String,
-    title: String,
-    year: String,
-    genres: String,
-    plot: String,
-    image: String,
-  }
-);
+const movieSchema = new mongoose.Schema({
+  id: String,
+  title: String,
+  year: String,
+  genres: String,
+  plot: String,
+  image: String         
 
-const usedmovieSchema = new mongoose.Schema({
+});
+
+const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+
+  favoriteMovies: [],  //added this for saving movies under a user
 });
 
 const Movie = mongoose.model("Movie", movieSchema);
 
-const Usedmovie = mongoose.model("Usedmovie", usedmovieSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports = Movie;
-module.exports = Usedmovie;
+module.exports = { Movie, User };
